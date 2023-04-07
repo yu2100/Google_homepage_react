cffc5de893aa88683d42e68c7d0044aea21fef20
Uloma <47474481+yu2100@users.noreply.github.com>
2023-04-07 01:46:48 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,33 @@
+/*eslint-disable jsx-a11y/anchor-is-valid*/
 import React from 'react';
+import LeafIcon from '../assets/leaf.png';
+import Location from './Location';
+import Links from './FooterLinks';
 
 const Footer = () => {
-    return 
-    <>
-    <h1>This is the Footer </h1>
-    </>
+  return (
+    <div className="footer">
+      <Location locate={'Nigeria'} />
+
+      <div className="footer_links">
+        <ul className="first_link">
+          <Links link={'About'} />
+          <Links link={'Advertising'} />
+          <Links link={'Business'} />
+          <Links link={'How Search Works'} />
+        </ul>
+        <div className="carbon">
+          <img src={LeafIcon} alt="leaf-icon " />
+          <p>Carbon neutral since 2007</p>
+        </div>
+        <ul className="second_link">
+          <Links link={'Privacy'} />
+          <Links link={'Terms'} />
+          <Links link={'Settings'} />
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
