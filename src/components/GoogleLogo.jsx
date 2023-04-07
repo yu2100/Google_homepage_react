6f47cdc6119e5e2a9110aa534b69e2a02b51e61c
Uloma <47474481+yu2100@users.noreply.github.com>
2023-04-07 01:49:14 +0100

    Create GoogleLogo.jsx

---
+++ src/components/GoogleLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../assets/google-logo.png';
+
+const GoogleLogo = () => {
+  return (
+    <div className="logo">
+      <img src={Logo} alt="google-log" />
+    </div>
+  );
+};
+
+export default GoogleLogo;
